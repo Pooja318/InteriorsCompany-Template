9292b238538fd097fe9b9fcd8a2bae5806d57fdc
Pooja318 <das.pooja318@gmail.com>
2017-11-09 00:24:39 +0530

    Initial commit

---
+++ scripts/interiors.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+//    $(".title").lettering();
+    $(".title").lettering('words').children("span").lettering();;
+    
+//    $(".title").lettering('words');
+})
+
+$(document).ready(function(){
+    $(".logo").lettering();
+})
+
+$(window).load(function(){
+//    $(".icons").sticky({ topSpacing: 0 });
+    $(".myNav").sticky({ topSpacing: 0});
+});
+
